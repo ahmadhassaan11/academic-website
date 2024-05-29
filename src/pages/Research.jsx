843f52ef320db29ac60840a12332716109d9224d
ahmadhassaan11 <ahmadullah943@.com>
2024-05-29 17:10:42 +0500

    minor design changes

--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Research = () => {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold mb-4">Research</h1>
+      {/* <h1 className="text-xl text-maincolor font-bold mb-4">Research</h1> */}
       <section className="mb-8">
-        <h2 className="text-2xl font-bold mb-2">Working Papers:</h2>
+        <h2 className="text-lg font-bold mb-2">Working Papers:</h2>
         <div className="mb-4">
-          <a className="text-xl font-semibold underline hover:text-maincolor" href="https://hu-my.sharepoint.com/personal/mbarrozo_g_harvard_edu/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fmbarrozo%5Fg%5Fharvard%5Fedu%2FDocuments%2FResearch%2Fbarrozo%5Fjmp%2Epdf&parent=%2Fpersonal%2Fmbarrozo%5Fg%5Fharvard%5Fedu%2FDocuments%2FResearch&ga=1">Market Power and Carbon Emissions in the Amazon [JOB MARKET PAPER]</a>
+          <a className="text-lg font-semibold underline hover:text-maincolor" href="https://hu-my.sharepoint.com/personal/mbarrozo_g_harvard_edu/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fmbarrozo%5Fg%5Fharvard%5Fedu%2FDocuments%2FResearch%2Fbarrozo%5Fjmp%2Epdf&parent=%2Fpersonal%2Fmbarrozo%5Fg%5Fharvard%5Fedu%2FDocuments%2FResearch&ga=1">Market Power and Carbon Emissions in the Amazon [JOB MARKET PAPER]</a>
           <p>
             The beef cattle sector is the leading driver of deforestation worldwide and a major source of carbon emissions.
             In this paper, I study how market power in the cattle supply chain shapes production and emissions in the Brazilian Amazon.
@@ -22,13 +22,13 @@ const Research = () => {
         </div>
       </section>
       <section className="mb-8">
-        <h2 className="text-2xl font-bold mb-2">In Progress:</h2>
+        <h2 className="text-lg font-bold mb-2">In Progress:</h2>
         <div className="mb-4">
-          <h3 className="text-lg font-semibold">Fidelity in Zero-Deforestation Supply Chains (joint with M. Skidmore, H. Gibbs, L. Rausch)</h3>
+          <h3 className="text-lg font-semibold hover:text-maincolor">Fidelity in Zero-Deforestation Supply Chains (joint with M. Skidmore, H. Gibbs, L. Rausch)</h3>
           <p className="italic">[Draft coming soon!]</p>
         </div>
         <div className="mb-4">
-          <h3 className="text-lg font-semibold">Property Rights and the Dynamics of Deforestation</h3>
+          <h3 className="text-lg font-semibold hover:text-maincolor">Property Rights and the Dynamics of Deforestation</h3>
         </div>
       </section>
     </div>
