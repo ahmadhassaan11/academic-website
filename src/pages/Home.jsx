01a457fe542b2c071ffb9c4de9b3a64f984a4699
ahmadhassaan11 <ahmadullah943@.com>
2024-06-03 13:30:27 +0500

    CI/CD Pipeline setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
                         alt="Profile Pic"
                         className="rounded-full w-64 h-64 md:w-80 md:h-80 object-cover mb-4"
                     />
-                    <h2 className="text-3xl font-bold mb-2">Marcos Barrozo Testing 123</h2>
+                    <h2 className="text-3xl font-bold mb-2">Marcos Barrozo</h2>
                     <h3 className="text-xl">Assistant Professor of Economics</h3>
                     <h3 className="text-xl">DePaul University</h3>
                 </div>
@@ -25,7 +25,7 @@ const Home = () => {
                     <h2 className="pt-4 text-lg font-bold text-left">Research</h2>
                     <DropdownCard
                         heading="Market Power and Carbon Emissions in the Amazon"
-                        titles={[{ text: 'paper', link: '/research' }]}
+                        titles={[{ text: 'paper', link: 'https://hu-my.sharepoint.com/personal/mbarrozo_g_harvard_edu/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fmbarrozo%5Fg%5Fharvard%5Fedu%2FDocuments%2FResearch%2Fbarrozo%5Fjmp%2Epdf&parent=%2Fpersonal%2Fmbarrozo%5Fg%5Fharvard%5Fedu%2FDocuments%2FResearch&ga=1' }]}
                         description="The beef cattle sector is the leading driver of deforestation worldwide and a major source of carbon emissions. In this paper, I study how market power in the cattle supply chain shapes production and emissions in the Brazilian Amazon. Intermediaries exert buyer (monopsony) power over farmers, but market structure varies geographically. Using rich transaction-level data, I find that large intermediaries extract monopsony markdowns on farmer prices in core regions, but the deforestation frontier is largely competitive. I estimate a quantitative spatial model of the cattle supply chain and find that monopsony power distorts output downward. This affects farmers negatively, but the greatest sources of emissions along the frontier remain unaffected. In counterfactual analysis, policies that target the largest firms or exporters are ineffective because they miss deforestation hotspots and burden farmers that already face market power. However, a combination of targeted production subsidies and a carbon tax can reallocate production away from the frontier and reduce emissions by a third while keeping beef production constant."
                     />
                     <DropdownCard
--- src/pages/Research.jsx
+++ src/pages/Research.jsx
@@ -10,7 +10,7 @@ const Research = () => {
         <div className="mb-4">
           <DropdownCard
             heading="Market Power and Carbon Emissions in the Amazon"
-            titles={[{ text: 'paper', link: '/research' }]}
+            titles={[{ text: 'paper', link: 'https://hu-my.sharepoint.com/personal/mbarrozo_g_harvard_edu/_layouts/15/onedrive.aspx?id=%2Fpersonal%2Fmbarrozo%5Fg%5Fharvard%5Fedu%2FDocuments%2FResearch%2Fbarrozo%5Fjmp%2Epdf&parent=%2Fpersonal%2Fmbarrozo%5Fg%5Fharvard%5Fedu%2FDocuments%2FResearch&ga=1' }]}
             description="The beef cattle sector is the leading driver of deforestation worldwide and a major source of carbon emissions. In this paper, I study how market power in the cattle supply chain shapes production and emissions in the Brazilian Amazon. Intermediaries exert buyer (monopsony) power over farmers, but market structure varies geographically. Using rich transaction-level data, I find that large intermediaries extract monopsony markdowns on farmer prices in core regions, but the deforestation frontier is largely competitive. I estimate a quantitative spatial model of the cattle supply chain and find that monopsony power distorts output downward. This affects farmers negatively, but the greatest sources of emissions along the frontier remain unaffected. In counterfactual analysis, policies that target the largest firms or exporters are ineffective because they miss deforestation hotspots and burden farmers that already face market power. However, a combination of targeted production subsidies and a carbon tax can reallocate production away from the frontier and reduce emissions by a third while keeping beef production constant."
           />
           <DropdownCard
