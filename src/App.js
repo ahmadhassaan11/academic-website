1a072e7ace3e05f8af4e31c0b0ff4c42d6eb174e
ahmadhassaan11 <ahmadullah943@.com>
2024-06-06 04:20:43 +0500

    fix: removed extra route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/academic-website/" element={<Home />} />
           <Route path="/research" element={<Research />} />
           <Route path="/teaching" element={<Teaching />} />
         </Routes>
