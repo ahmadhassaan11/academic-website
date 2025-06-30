edd3083da383518ed9832e92870edc960463fcc6
mbarrozof <mbarrozofilho@gmail.com>
2025-06-30 17:23:46 -0500

    Update Teaching.jsx

--- src/pages/Teaching.jsx
+++ src/pages/Teaching.jsx
@@ -6,14 +6,14 @@ const Teaching = () => {
       {/* <h1 className="text-xl text-maincolor font-bold mb-4">Teaching</h1> */}
       <section className="mb-8">
         <h2 className="text-[19px] font-bold mb-2">Courses taught:</h2>
-          <ul className="list-disc list-inside ml-5">
-            <li>Empirical Industrial Organization (DePaul MS, 2025).</li>
-            <li>Principles of Microeconomics (DePaul, 2024-).</li>
-            <li>Environmental Economics in the Developing World (Harvard, 2021 and 2022).</li>
-            <li>Political Economy of Globalization (Teaching Fellow at Harvard, 2020 and 2021).</li>
-          </ul>
-        </div>
+        <ul className="list-disc list-inside ml-5">
+          <li>Empirical Industrial Organization (DePaul MS, 2025).</li>
+          <li>Principles of Microeconomics (DePaul, 2024–).</li>
+          <li>Environmental Economics in the Developing World (Harvard, 2021 and 2022).</li>
+          <li>Political Economy of Globalization (Teaching Fellow at Harvard, 2020 and 2021).</li>
+        </ul>
       </section>
+    </div>
   );
 };
 
